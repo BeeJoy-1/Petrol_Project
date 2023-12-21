bde7da9dcdbf483bad7d2883f229a55f766a5361
BeeJoy-1 <beejoy3000@gmail.com>
2023-12-21 11:46:23 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        headerbg: "#282828",
+        primary: "#F40404",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamily: {
+        'poppins': "'Poppins', sans-serif",
+      },
+      backgroundImage: {
+        'banner': " linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('./images/banner.jpg')",
+      }
+    }
+  }
+}
